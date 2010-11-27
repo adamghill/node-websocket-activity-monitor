57b8db417b870cfd981bf82e1347e93efcb831cb
Adam Hill <adamghill@gmail.com>
2010-11-27 10:33:07 -0500

    Fix include. Only get the disk0 data.

--- server/activity-monitor.js
+++ server/activity-monitor.js
@@ -1,8 +1,8 @@
 var sys = require('sys')
   , http = require("http")
-  , ws = require('./node-websocket-server/lib/ws');
+  , ws = require('./node-websocket-server/lib/ws/server.js');
 
-var iostat = require('child_process').spawn("iostat", ["-w 1"]);
+var iostat = require('child_process').spawn("iostat", ["-w 1", "disk0"]);
 
 var httpServer = http.createServer();
 
@@ -10,57 +10,72 @@ var server = ws.createServer({
   debug: true
 }, httpServer);
 
-function format (data) {
+function format(data) {
   var output_data = data.toString();
   sys.log(output_data);
   header = 'disk0       cpu     load average'
+  
   if (output_data.match(header)) {
     sys.log("ignore header")
-  }else{
-    // disk0 cpu load 
+  } else {
+    // disk0 cpu load
     // average kbt tps kbs us sy id 1m 5m 15m
     var output_array = output_data.replace(/^\s+|\s+$/g,"").split(/\s+/);
-    for (var i=0; i < output_array.length; i++) {
-      output_array[i] = parseFloat( output_array[i]);
-    };
-    output_hash = {
-      date:new Date(),
-      disk:{
-        kbt:output_array[0],
-        tps:output_array[1],
-        mbs:output_array[2]
-      },
-      cpu:{
-        us:output_array[3],
-        sy:output_array[4],
-        id:output_array[5]
-      },
-      load_average:{
-        m1:output_array[6],
-        m5:output_array[7],
-        m15:output_array[8]
+    
+    if (output_array) {    
+      for (var i=0; i < output_array.length; i++) {
+        output_array[i] = parseFloat(output_array[i]);
+      };
+      
+      var array_len = output_array.length;
+      
+      output_hash = {
+        date: new Date(),
+        disk0: {
+          kbt: output_array[array_len-9],
+          tps: output_array[array_len-8],
+          mbs: output_array[array_len-7],
+        },
+        cpu: {
+          us: output_array[array_len-6],
+          sy: output_array[array_len-5],
+          id: output_array[array_len-4],
+        },
+        load_average: {
+          m1: output_array[array_len-3],
+          m5: output_array[array_len-2],
+          m15: output_array[array_len-1],
+        }
       }
+      
+      return JSON.stringify(output_hash);
     }
-    return JSON.stringify(output_hash);
   }
 }
 
 // Handle WebSocket Requests
-server.addListener("connection", function(conn){
-  sys.log("opened connection: "+conn.id);
-  server.send(conn.id, "Connected as: "+conn.id);
-  iostat.stdout.on('data', function (data) {
-    sys.log(typeof(data));
-    sys.log('stdout: ' + data);
-    sys.log('stdout: ' + format(data));
-    server.send(conn.id, format(data));
-  });
-  server.send(conn.id, "Connected as: "+conn.id);
+server.addListener("connection", function(conn) {
+  sys.log("Opened connection: " + conn.id);
+  server.send(conn.id, "Connected as: " + conn.id);
+  
+  if (iostat.pid) {
+    sys.log('Process pid: ' + iostat.pid)
+  
+    iostat.stdout.on('data', function(data) {
+      //sys.log('Process stdout: ' + data);
+      sys.log('Formatted stdout: ' + format(data));
+      server.send(conn.id, format(data));
+    });
+  } else {
+    sys.log("Process failed to spawn")
+  }
+  
+  server.send(conn.id, "Connected as: " + conn.id);
 });
 
-server.addListener("close", function(conn){
-  sys.log("closed connection: "+conn.id);
-  conn.broadcast("<"+conn.id+"> disconnected");
+server.addListener("close", function(conn) {
+  sys.log("Closed connection: " + conn.id);
+  conn.broadcast("<" + conn.id + "> disconnected");
 });
 
 server.listen(8000);
